f503cfdbe3577bcc25e849626a9d2a184ac6a3c5
ali langah <alilangah78@gmail.com>
2024-09-04 12:37:31 +0500

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link"
 export default function Dashboard() {
   return (
-    <div className="flex items-center">
+    <div className="flex flex-col p-8 items-center">
       <h1 className="text-2xl font-extrabold">dashboard</h1>
       <Link href={'/register'}><h1 className="text-xl font-bold">logout</h1></Link>
     </div>
